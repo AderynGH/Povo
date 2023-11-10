6c02c73462799a0ada268c34cef776d78dceffd3
Celeste Artley <90732656+CelesteGiraffe@users.noreply.github.com>
2023-11-10 19:50:30 +0000

    Updated the basic objects for actors and display classes

--- src/Enemy.lua
+++ src/Enemy.lua
@@ -2,12 +2,12 @@ local Actress = require("Actress")
 
 local Character = Actress:new()
 
-function Character:new(o, name, hp, speed)
+function Character:new(o, name, type, hp, speed)
     o = o or Actress:new(o)
     setmetatable(o, self)
     self.__index = self
-    self.type = "Character"
-    self.name = "Player"
+    self.type = type
+    self.name = name or "DefaulEnemy"
     self.hp = hp or 100
     self.speed = speed or 10
     self.image = "assets/character.png"
