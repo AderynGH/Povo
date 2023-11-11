de83e0098d5abbf86ad89e8190251f00aa71e133
AderynGH <131829887+AderynGH@users.noreply.github.com>
2023-11-11 13:12:04 +0000

    Added newAnimation constructor

--- src/Actress.lua
+++ src/Actress.lua
@@ -1,4 +1,5 @@
 local Displayable = require("Displayable")
+local SpriteManger = require("SpriteManager")
 
 local Actress = Displayable:new()
 
@@ -13,8 +14,27 @@ function Actress:new(o, type, name, hp, speed)
 
     return o
 end
-function Actress:die(deathAnimation)
-    -- death code here
+
+function Actress:newAnimation(o, aniName, d, idle, sf, aniL, spd, aniSprites)
+    o = o or Actress:new(o)
+    setmetatable(o, self)
+    self.__index = self
+    self.animationName = aniName or "default"
+    self.direction = d or "right"
+    self.idle = idle or true
+    self.startFrame = sf or 1
+    self.animationLength = aniL or 1
+    self.animationSpeed = spd or 20
+    self.animationTimer = 0.1
+    self.sprite = love.graphics.newImage(aniSprites) or
+    love.graphics.newImage("default.png")
+    --[[self.spriteWidth = SpriteManger:getSpriteWidth(aniSprites) or 64
+    self.spriteHeight = getSpriteHeight(aniSprites) or 64
+    self.quadWidth = getQuadWidth(spriteWidth) or 64
+    self.quadHeight = getQuadHeight(spriteHeight) or 64]]
+    self.quads = {}
+
+    return o
 end
 
 return Actress
